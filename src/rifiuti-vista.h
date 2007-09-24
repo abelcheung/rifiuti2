da2d8fdf14921bd74fee2aa76b3de41286a8b2a2
Abel Cheung <abelcheung@gmail.com>
2007-09-24 13:36:54 +0000

    2007-09-24  Abel Cheung  <abelcheung@gmail.com>
    	* src/rifiuti-vista.c, src/rifiuti-vista.h:
    	- New version for Vista recycle bin analysis.

---
+++ src/rifiuti-vista.h
@@ -0,0 +1,24 @@
+#ifndef _RIFIUTI_VISTA_H
+#define _RIFIUTI_VISTA_H
+
+#include <stdint.h>
+#include <time.h>
+
+enum {
+  FILESIZE_OFFSET = 0x8,
+  FILETIME_OFFSET = 0x10,
+  FILENAME_OFFSET = 0x18
+};
+
+struct _rbin_struct {
+  uint64_t   filesize;
+  struct tm *filetime;
+  char      *utf8_filename;
+};
+
+typedef struct _rbin_struct rbin_struct;
+
+#endif
+
+/* vi: shiftwidth=2 expandtab tabstop=2
+ */
