ddcdd0e159f25810282522a399ef8133986d2f0a
Abel Cheung <abelcheung@gmail.com>
2019-03-02 12:23:02 +0800

    Appveyor zip filename depends on tag status

--- appveyor.bat
+++ appveyor.bat
@@ -1,6 +1,9 @@
 @echo off
-:: Copyright 2018 Abel Cheung
+:: Copyright 2018-19 Abel Cheung
 :: License same as main package
+
+set OLDPATH=%PATH%
+PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%OLDPATH%
 
 cd %APPVEYOR_BUILD_FOLDER%
 goto %1
@@ -12,16 +15,14 @@ exit 1
 :build
 @echo on
 
-set OLDPATH=%PATH%
-PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%OLDPATH%
-
 :: Build fails (unresolved dep) w/o updated msys2 runtime :-/
-C:\msys64\usr\bin\pacman.exe --noconfirm --noprogressbar -Syu
+pacman.exe --noconfirm --noprogressbar -Syu
 
+:: Second invocation updates everything else
 :: Wastes too much time updating unnecessary non-core stuff
-:: bash -lc "pacman --noconfirm --noprogressbar -Syu"
+:: pacman --noconfirm --noprogressbar -Syu
 
-C:\msys64\usr\bin\pacman.exe --noconfirm --noprogressbar -S --needed markdown mingw-w64-%MSYS2_ARCH%-glib2
+pacman.exe --noconfirm --noprogressbar -S --needed markdown mingw-w64-%MSYS2_ARCH%-glib2
 bash -lc "./autogen.sh && ./configure --enable-static && make -C po rifiuti.pot && make all"
 
 @echo off
@@ -31,11 +32,11 @@ goto :eof
 :check
 @echo on
 
-c:\msys64\usr\bin\file.exe src\rifiuti.exe
-c:\msys64\usr\bin\ldd.exe  src\rifiuti.exe
+file.exe src\rifiuti.exe
+ldd.exe  src\rifiuti.exe
 
-c:\msys64\usr\bin\file.exe src\rifiuti-vista.exe
-c:\msys64\usr\bin\ldd.exe  src\rifiuti-vista.exe
+file.exe src\rifiuti-vista.exe
+ldd.exe  src\rifiuti-vista.exe
 
 bash -lc "make check"
 
@@ -46,9 +47,12 @@ goto :eof
 :package
 @echo on
 
-bash -lc "make dist-win"
-
-:: todo rename daily builds, publish artifact
+if "%APPVEYOR_REPO_TAG%" == "true" (
+    echo "*** Building official release ***"
+    bash -lc "make dist-win"
+) else (
+    bash -lc "make dist-win ZIPNAME=%APPVEYOR_PROJECT_SLUG%-%APPVEYOR_REPO_COMMIT:~0,8%-win-%MSYS2_ARCH%"
+)
 
 @echo off
 goto :eof
